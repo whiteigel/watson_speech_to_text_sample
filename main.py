c3f3535a3bbfa2048d4f92d37614a511d894eb05
Sergey Volodin <whiteigel@gmail.com>
2021-07-30 15:18:33 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from ibm_watson import SpeechToTextV1
+from ibm_watson import LanguageTranslatorV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+
+S2T_API = os.environ.get('S2T_API')
+LT_API = os.environ.get('LT_API')
+
+S2T_URL = 'https://api.eu-de.speech-to-text.watson.cloud.ibm.com/instances/a7d6af82-870d-44b1-86dd-2ce182b91144'
+LT_URL = 'https://api.eu-de.language-translator.watson.cloud.ibm.com/instances/d9369b3b-9ec6-455a-887c-f6888fc33599'
+FILENAME = 'PolynomialRegressionandPipelines.mp3'
+version_lt = '2018-05-01'
+
+
+def speech_to_text(key, url, file):
+    authenticator = IAMAuthenticator(key)
+    s2t = SpeechToTextV1(authenticator=authenticator)
+    s2t.set_service_url(url)
+    with open(file, mode="rb") as wav:
+        response = s2t.recognize(audio=wav, content_type='audio/mp3')
+        res = response.result
+    recognized_text = res['results'][0]["alternatives"][0]["transcript"]
+    print(recognized_text)
+    return recognized_text
+
+
+def lang_translator(key, url, text, lang):
+    authenticator = IAMAuthenticator(key)
+    language_translator = LanguageTranslatorV3(version=version_lt, authenticator=authenticator)
+    language_translator.set_service_url(url)
+    translation_response = language_translator.translate(text=text, model_id=f'en-{lang}')
+    translation = translation_response.get_result()
+    russian_translation = translation['translations'][0]['translation']
+    print(russian_translation)
+    return russian_translation
+
+
+if __name__ == '__main__':
+    LANG = input(str('Choose a language to translate to: '))
+    lang_translator(LT_API, LT_URL, speech_to_text(S2T_API, S2T_URL, FILENAME), LANG)
